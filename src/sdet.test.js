1faddf52a96508feb5c5f59cd9265537959d655a
Andy Philbert Jr <InRhythm@admins-air.lan>
2018-09-17 11:18:11 -0400

    added test to confirm if input is a string

--- src/sdet.test.js
+++ src/sdet.test.js
@@ -4,6 +4,7 @@ test('Find the longest word in the following sentence', () => {
     expect(newSentence.findLongestWord('The cow jumped over the moon.')).toBe('jumped 6')
 })
 
-//test('is the argument being passed in a string'), () => {
-  //  expect(newSentence.findLongestWord('The cow jump over the moon.')).toBe()
-//}
+test('is the argument being passed in a string', () => {
+   expect(typeof 'value').toBe("string")
+})
+
